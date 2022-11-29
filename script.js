3adda7af058c04d8c693363973611321408d2c8a
KatrinaGandzjuka <katrina.gandzjuk@icloud.com>
2022-11-29 20:28:08 +0200

    Nvembra darbini

---
+++ script.js
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+  $("#form").submit(function () {
+    var th = $(this);
+    $.ajax({
+      type: "POST",
+      url: "mail.php",
+      data: th.serialize(),
+    }).done(function () {
+      alert("Thank you!");
+      setTimeout(function () {
+        th.trigger("reset");
+      }, 1000);
+    });
+    return false;
+  });
+});
